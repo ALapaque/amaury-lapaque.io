61c9c4c1a03923c62a661ce1d4ba85520643e562
Amaury <amaury.lapaque@gmail.com>
2022-09-28 22:34:59 +0200

    added new header backdrop filter

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -5,42 +5,44 @@ import HeaderSocialIcons from './HeaderSocialIcons';
 
 const Header = () => {
   return (
-    <header className={ 'sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center' }>
-      <motion.div
-        initial={ {
-          x: -500,
-          opacity: 0,
-          scale: 0.5
-        } }
-        animate={ {
-          x: 0,
-          opacity: 1,
-          scale: 1
-        } }
-        transition={ {
-          duration: 0.75
-        } }
-        className={ 'flex flex-row items-center' }>
-        <HeaderSocialIcons />
-      </motion.div>
+    <header className={ 'sticky top-0 z-50 w-full flex backdrop-blur-xl' }>
+      <div className={'w-full p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center'}>
+        <motion.div
+          initial={ {
+            x: -500,
+            opacity: 0,
+            scale: 0.5
+          } }
+          animate={ {
+            x: 0,
+            opacity: 1,
+            scale: 1
+          } }
+          transition={ {
+            duration: 0.75
+          } }
+          className={ 'flex flex-row items-center' }>
+          <HeaderSocialIcons />
+        </motion.div>
 
-      <motion.div
-        initial={ {
-          x: 500,
-          opacity: 0,
-          scale: 0.5
-        } }
-        animate={ {
-          x: 0,
-          opacity: 1,
-          scale: 1
-        } }
-        transition={ {
-          duration: 0.75
-        } }
-        className={ 'text-gray-300' }>
-        <HeaderContactIcon />
-      </motion.div>
+        <motion.div
+          initial={ {
+            x: 500,
+            opacity: 0,
+            scale: 0.5
+          } }
+          animate={ {
+            x: 0,
+            opacity: 1,
+            scale: 1
+          } }
+          transition={ {
+            duration: 0.75
+          } }
+          className={ 'text-gray-300' }>
+          <HeaderContactIcon />
+        </motion.div>
+      </div>
     </header>
   )
     ;
