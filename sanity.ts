11f27d35af4f36db1f57061136dbd8270dac744f
Amaury <amaury.lapaque@gmail.com>
2022-09-30 23:44:27 +0200

    added link to sanity

--- sanity.ts
+++ sanity.ts
@@ -4,7 +4,7 @@ import { createClient } from 'next-sanity';
 
 export const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: '53g7za50',
   apiVersion: '2021-03-25',
   useCdn: process.env.NODE_ENV === 'production'
 };
