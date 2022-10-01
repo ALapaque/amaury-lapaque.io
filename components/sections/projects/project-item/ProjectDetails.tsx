b4ed80eb96af19b94d80e663b453f98654e7079c
Amaury <amaury.lapaque@gmail.com>
2022-10-01 17:49:47 +0200

    update style

--- components/sections/projects/project-item/ProjectDetails.tsx
+++ components/sections/projects/project-item/ProjectDetails.tsx
@@ -9,7 +9,7 @@ const ProjectDetails = ({ project }: Props) => {
   return (
     <div className={ 'space-y-10 px-0 md:px-10 max-w-6xl' }>
       <h4 className={ 'text-4xl font-semibold text-center' }>{ project.title }</h4>
-      <p className={ 'text-lg text-center md:text-left' }>{ project.summary }</p>
+      <p className={ 'text-lg text-left' }>{ project.summary }</p>
       <div className={ 'grid grid-cols-4 md:grid-cols-6 gap-2 justify-items-center' }>
         { project.technologies.map((skill: Skill, index: number) => (
           <WorkExperienceSkill key={ index }
