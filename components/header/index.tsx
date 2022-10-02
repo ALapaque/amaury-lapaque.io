5f8648f488c100d6efed1ec4806e85eb614586cf
Amaury <amaury.lapaque@gmail.com>
2022-10-02 18:12:57 +0200

    hidden on mobile

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,12 +1,11 @@
 import { motion } from 'framer-motion';
 import HeaderContactIcon from './HeaderContactIcon';
-import HeaderSocialIcon from './HeaderSocialIcon';
 import HeaderSocialIcons from './HeaderSocialIcons';
 
 const Header = () => {
   return (
-    <header className={ 'sticky top-0 z-50 w-full flex backdrop-blur-xl' }>
-      <div className={'w-full p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center'}>
+    <header className={ 'sticky top-0 z-50 w-full flex' }>
+      <div className={ 'w-full p-5 flex items-start justify-between max-w-7xl mx-auto xl:items-center' }>
         <motion.div
           initial={ {
             x: -500,
