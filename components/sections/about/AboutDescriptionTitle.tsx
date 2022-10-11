cc338d93fc0f62fb74bef6a631be93b1d9034ca2
Amaury <amaury.lapaque@gmail.com>
2022-10-11 18:38:23 +0200

    new design for underline word

--- components/sections/about/AboutDescriptionTitle.tsx
+++ components/sections/about/AboutDescriptionTitle.tsx
@@ -3,7 +3,7 @@ const AboutDescriptionTitle = () => {
     <h4 className={ 'text-4xl font-semibold text-[color:var(--text)]' }>
       Here is a&nbsp;
       <span className="before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-primary/60 relative inline-block">
-        <span className="relative text-[color:var(--background)]">annoyed</span>
+        <span className="relative text-[color:var(--background)]">little</span>
       </span>
       &nbsp;background
     </h4>
