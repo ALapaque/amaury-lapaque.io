9bb2cfe8ea1e31d73d591f4a68d802c56e2da4c7
Amaury <amaury.lapaque@gmail.com>
2022-10-13 20:12:58 +0200

    added services page

--- components/sections/service/service-item/index.tsx
+++ components/sections/service/service-item/index.tsx
@@ -10,10 +10,8 @@ const ServiceItem = ({ service }: Props) => {
     <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
       <div className="mb-12 space-y-4">
         <h3 className="text-2xl font-semibold text-[color:var(--primary)]">{ service.name }</h3>
-        <p className="mb-6 text-[color:var(--background)]">
-          { service.description }
-
-        </p>
+        <p className="mb-6 text-[color:var(--background)]"
+           dangerouslySetInnerHTML={ { __html: service.description } } />
       </div>
       <CustomSVG className="w-2/3 ml-auto -mb-32 -mr-12"
                  __html={ service.svg }
