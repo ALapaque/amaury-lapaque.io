5292b52e17b449809ea870e810001cbe917d78e9
Amaury <amaury.lapaque@gmail.com>
2022-10-13 20:19:06 +0200

    added services page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import type { AppProps } from 'next/app';
+import { useEffect } from 'react';
+import ReactGA from 'react-ga';
 import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
+ReactGA.initialize('G-PL59EV0Q63');
+
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <RecoilRoot>
       <Component { ...pageProps } />
