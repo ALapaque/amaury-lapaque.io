718c81d440117516a309aaf621c8682973d612a0
Amaury <amaury.lapaque@gmail.com>
2022-10-15 16:26:51 +0200

    Added react-ga react-gtm

--- hooks/useEventTracker.ts
+++ hooks/useEventTracker.ts
@@ -5,7 +5,9 @@ import TagManager from 'react-gtm-module';
 
 const useEventTracker = () => {
   useEffect(() => {
-    if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID && process.env.NODE_ENV !== 'production') {
+    if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID &&
+        !process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID &&
+        process.env.NODE_ENV !== 'production') {
       return;
     }
 
