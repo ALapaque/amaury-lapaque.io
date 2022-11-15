f22e4b45b3f8af6a7fb8d22c8d3260d4d7d213a2
Amaury <amaury.lapaque@gmail.com>
2022-11-15 20:12:05 +0100

    added custom header for vercel

--- next.config.js
+++ next.config.js
@@ -11,6 +11,21 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['cdn.sanity.io']
+  },
+  async headers() {
+    const headers = [];
+    if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'preview') {
+      headers.push({
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'noindex'
+          }
+        ],
+        source: '/:path*'
+      });
+    }
+    return headers;
   }
 };
 
