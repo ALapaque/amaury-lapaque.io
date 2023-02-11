b7ee5c96984fcc5f45a0a6c51d1edca8fe06691e
Amaury <amaury.lapaque@gmail.com>
2023-02-11 14:40:41 +0100

    adding argos CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  projectId: '9t35ov',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
