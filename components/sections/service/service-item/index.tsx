4ca40ceec31ee08c2be3fb4d8502f74f5019a835
Amaury Lapaque <46450511+ALapaque@users.noreply.github.com>
2023-02-18 10:17:32 +0100

    Feature/animation (#15)
    * fix scroll anchor behavior
    
    * fix scroll anchor behavior
    
    * fix scroll anchor behavior
    
    * added new theme button
    
    * Added lottie

--- components/sections/service/service-item/index.tsx
+++ components/sections/service/service-item/index.tsx
@@ -1,7 +1,9 @@
+import { Player } from '@lottiefiles/react-lottie-player';
+import { AnimationItem } from 'lottie-web';
+import { useEffect, useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { pageInfoSelector } from '../../../../stores/data';
 import { Service } from '../../../../typing';
-import CustomSVG from '../../../CustomSVG';
 import ServiceItemDescription from './ServiceItemDescription';
 import ServiceItemLink from './ServiceItemLink';
 import ServiceItemTitle from './ServiceItemTitle';
@@ -11,14 +13,34 @@ type Props = {
 }
 
 const ServiceItem = ({ service }: Props) => {
+  const [ lottieRef, setLottieRef ] = useState<AnimationItem | undefined>();
   const pageInfo = useRecoilValue(pageInfoSelector);
+
+  const _goToLastFrame = () => {
+    lottieRef && lottieRef?.goToAndStop(lottieRef?.totalFrames - 1, true);
+  };
+
+  const _handleOnMouseEnter = () => {
+    lottieRef && lottieRef.play();
+  };
+
+  const _handleOnMouseLeave = () => {
+    lottieRef && lottieRef.pause();
+    _goToLastFrame();
+  };
+
+  useEffect(() => {
+    _goToLastFrame();
+  }, [ lottieRef ]);
 
   if (!pageInfo) {
     return <></>;
   }
 
   return (
-    <div className={ 'group border-none transition  rounded-md text-[color:var(--text)] transition bg-[color:var(--primary-70)]' +
+    <div onMouseEnter={ _handleOnMouseEnter }
+         onMouseLeave={ _handleOnMouseLeave }
+         className={ 'group border-none transition  rounded-md text-[color:var(--text)] transition bg-[color:var(--primary-70)]' +
                      ' hover:z-[1] hover:shadow-2xl hover:cursor-pointer' }>
       <div className={ 'h-full flex flex-col justify-between p-8 space-y-8 border-dashed rounded-lg transition duration-300' +
                        ' text-[color:var(--background)]' +
@@ -28,8 +50,11 @@ const ServiceItem = ({ service }: Props) => {
                        ' group-hover:scale-90' }>
         <div className="space-y-2">
           <div className={ 'w-full flex justify-end' }>
-            <CustomSVG __html={ service.svg }
-                       className={ 'h-20 w-20 md:w-40 md:h-40' }
+            <Player
+              lottieRef={ (item: AnimationItem) => setLottieRef(item) }
+              loop
+              src={ service.svg }
+              className={ 'h-20 w-20 md:w-40 md:h-40' }
             />
           </div>
 
