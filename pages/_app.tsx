5fdf22b57c04bad5d302dd6c0260910dfdd55189
amaury <amaury.lapaque@gmail.com>
2023-03-08 19:07:19 +0100

    remove PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,46 @@
-import type { AppProps } from 'next/app';
+import type {AppProps} from 'next/app';
 import Head from 'next/head';
-import { RecoilRoot } from 'recoil';
+import {RecoilRoot} from 'recoil';
 import useEventTracker from '../hooks/useEventTracker';
 import '../styles/globals.css';
+import {useEffect} from "react";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  useEventTracker();
+function MyApp({Component, pageProps}: AppProps) {
+	useEventTracker();
 
-  return (
-    <>
-      <Head>
-        <meta name="google"
-              content="nositelinkssearchbox" />
-        <meta name="google"
-              content="nopagereadaloud" />
-        <meta name="viewport"
-              content="width=device-width, initial-scale=1" />
-        <meta name="google-site-verification"
-              content="bXCbkD-6EYFXtLD8Ol4rXdaE88T5KXHbwyWaTmxi6JM" />
-        <meta name="description"
-              content="Amaury Lapaque, Fullstack developer specialized in frontend technologies, feel free to contact me" />
-      </Head>
-      <RecoilRoot>
-        <Component { ...pageProps } />
-      </RecoilRoot>
-    </>
-  );
+	const unregister = () => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker.getRegistrations().then(function (registrations) {
+				for (const registration of registrations) {
+					registration.unregister()
+				}
+			})
+		}
+	}
+
+	useEffect(() => {
+		unregister()
+	}, [])
+
+	return (
+		<>
+			<Head>
+				<meta name="google"
+							content="nositelinkssearchbox"/>
+				<meta name="google"
+							content="nopagereadaloud"/>
+				<meta name="viewport"
+							content="width=device-width, initial-scale=1"/>
+				<meta name="google-site-verification"
+							content="bXCbkD-6EYFXtLD8Ol4rXdaE88T5KXHbwyWaTmxi6JM"/>
+				<meta name="description"
+							content="Amaury Lapaque, Fullstack developer specialized in frontend technologies, feel free to contact me"/>
+			</Head>
+			<RecoilRoot>
+				<Component {...pageProps} />
+			</RecoilRoot>
+		</>
+	);
 }
 
 export default MyApp;
