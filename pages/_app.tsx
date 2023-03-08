def2b80dc16ba995101446d18021b82e68555c76
amaury <amaury.lapaque@gmail.com>
2023-03-08 19:01:14 +0100

    remove PWA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,12 +10,6 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="manifest"
-              href="/manifest.json" />
-        <link rel="apple-touch-icon"
-              href="/icon.png"></link>
-        <meta name="theme-color"
-              content="#fff" />
         <meta name="google"
               content="nositelinkssearchbox" />
         <meta name="google"
